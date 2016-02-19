34b8f42a20c740d6038ffeea6228d3e5f68836b8
chenyun <corey611@outlook.com>
2016-02-19 23:43:32 +0800

    CmdDemo

--- MVVM_1_Demo/MVVM_1_Demo/OrderModel.cs
+++ MVVM_1_Demo/MVVM_1_Demo/OrderModel.cs
@@ -1,14 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MVVM_1_Demo
 {
-    public class OrderModel
+    public class OrderModel:INotifyPropertyChanged
     {
-        public string OrderID { get; set; }
-        public string OrderName { set; get; }
+        private string orderID;
+        public string OrderID
+        {   get { return orderID; }
+            set
+            {
+                if (orderID == value)
+                    return;
+                orderID = value;
+                NotifyPropertyChanged("OrderID");
+            }
+        }
+
+        private string orderName;
+        public string OrderName
+        {
+            get { return orderName; }
+            set
+            {
+                if (orderName == value)
+                    return;
+                orderName = value;
+                NotifyPropertyChanged("OrderName");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyname)
+        {
+            if (PropertyChanged != null )
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        } 
     }
 }
